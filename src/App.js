a1e6154557ff961de06cc4a26e7e05f1ed67d756
Elena Dyagileva <edyagileva96@gmail.com>
2023-02-20 09:26:02 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="my-container">
+      <ul>
+        <li class="my-item">
+          <img src="https://picsum.photos/200" alt="Random Image" />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            hendrerit rutrum velit vel interdum. Suspendisse condimentum arcu id
+            felis ullamcorper bibendum. Nulla tristique nunc quis mauris cursus,
+            eu posuere tellus ultrices. Praesent id lorem et lacus laoreet
+            tempor. Pellentesque euismod velit vel ipsum porttitor, at efficitur
+            ipsum euismod. Sed suscipit mauris a nibh venenatis bibendum.
+            Aliquam erat volutpat. Suspendisse potenti. Ut ullamcorper metus in
+            mi finibus, sed pellentesque velit porta. Sed eu magna nunc. Aenean
+            quis magna ac massa lacinia pellentesque. Rubicon.
+          </p>
+        </li>
+        <li class="my-item">
+          <img src="https://picsum.photos/200" alt="Random Image" />
+          <p>
+            Nullam euismod justo vel metus consequat, a lacinia ex sagittis.
+            Curabitur vel justo magna. Etiam tincidunt semper leo nec rhoncus.
+            Nunc accumsan mi nisl, id blandit est euismod id. Sed rutrum tellus
+            a luctus vehicula. Pellentesque habitant morbi tristique senectus et
+            netus et malesuada fames ac turpis egestas. Sed gravida, massa a
+            lacinia consequat, sapien sapien vehicula purus, id bibendum nulla
+            magna id ex. Rubicon. Fusce sagittis eu enim at dignissim. Mauris
+            blandit felis sed sem pharetra, vel feugiat urna dictum. Curabitur
+            accumsan convallis metus in semper. Nunc pulvinar tortor mauris, non
+            ullamcorper lorem aliquam non. Nam maximus, ex at convallis
+            porttitor, neque velit euismod orci, ut varius tellus mi non ipsum.
+            Vestibulum vel turpis quis odio semper aliquet. Aliquam eu dapibus
+            enim. Aenean ut felis sed nulla finibus elementum.
+          </p>
+        </li>
+        <li class="my-item">
+          <img src="https://picsum.photos/200" alt="Random Image" />
+          <p>
+            Pellentesque habitant morbi tristique senectus et netus et malesuada
+            fames ac turpis egestas. Vestibulum ante ipsum primis in faucibus
+            orci luctus et ultrices posuere cubilia curae; Sed nec gravida
+            metus, vel blandit velit. Ut ac purus nec augue efficitur pretium eu
+            vel arcu. Nam et justo faucibus, mollis velit ut, venenatis est. In
+            venenatis augue libero, in maximus elit placerat nec. In euismod mi
+            nisl, sit amet varius mauris pellentesque eget. Etiam vitae massa
+            felis. Vestibulum semper nunc ut ex euismod commodo. Rubicon. Fusce
+            eu eros pharetra, posuere eros nec, rutrum risus. Nulla facilisi.
+            Morbi commodo ipsum et arcu placerat auctor.
+          </p>
+        </li>
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
